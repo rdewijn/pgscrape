f5dbf9e885cddf5f66153ad042f3629c55b3b11f
Rik de Wijn <rdwijn@nl.pamgene.com>
2018-04-17 16:19:50 +0200

    180417

---
+++ 180417_PTK.R
@@ -0,0 +1,20 @@
+library(pgscrape)
+library(dplyr)
+
+p2p = read.table("Pep2Protein-Short-PTK-86312-86402-swiss_prot-HUMAN-2018-04-17.txt", header = TRUE, sep = "\t")
+
+p2p = p2p %>% distinct( PepProtein_PhosLink, ID, PepProtein_SeqHomology, .keep_all = TRUE)
+scrape = p2p %>% group_by(PepProtein_PhosLink, ID, PepProtein_SeqHomology) %>% do(
+  {
+    aProt = .$PepProtein_UniprotID
+    aSite =  paste(.$PepProtein_Residue, .$PepProtein_PhosSite, sep = "")
+    print(paste(aProt, aSite))
+    res = getPNetPredictions(uni = aProt,
+                             ps  = aSite)
+    if(is.null(res)){
+      res = data.frame()
+    }
+    res
+  }
+)
+save(file = "180417 scrape.RData", scrape)
